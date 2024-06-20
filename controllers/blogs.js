1397bd2efd07fddc3136b6e932707c918aadc6d1
Yousef Mahmoud <mryoyo133@gmail.com>
2024-06-20 22:02:36 +0300

    4.13 -> 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,4 +21,31 @@ blogRouter.post('/', async (request, response) => {
 
 })
 
+blogRouter.delete("/:id", async (req, res)=>{
+    try{
+        await Blog.findByIdAndDelete(req.params.id)
+        res.status(201).end()
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
+})
+
+blogRouter.put("/:id", async (req, res) => {
+    const body = req.body
+  
+    const blog = {
+      title: body.title,
+      url: body.url,
+      likes: body.likes
+    }
+    try{
+        let updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new:true, runValidators: true, context:"query"})
+        updatedBlog = res.status(200).json(updatedBlog)
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
+  })
+
 module.exports = blogRouter
--- tests/blogApi.test.js
+++ tests/blogApi.test.js
@@ -65,6 +65,53 @@ test("test for malformed input", async ()=>{
     await api.post("/api/blogs").send(noUrl).expect(400) 
 })
 
+test("delete a blog with valid id", async () =>{
+    let blog = (await api.get("/api/blogs")).body[0]
+    let id = blog.id
+    await api.delete(`/api/blogs/${id}`).expect(201)
+})
+
+test("try to delete a blog with invalid id", async () =>{
+    let id = '15315321'
+    await api.delete(`/api/blogs/${id}`).expect(400)
+})
+
+test("update a blog with valid id", async () =>{
+    let blog = (await api.get("/api/blogs")).body[0]
+    let id = blog.id
+
+    let newBlog = {
+        title:"something",
+        url:"https://something.someone.com",
+        likes: 30
+    }
+    await api.put(`/api/blogs/${id}`).send(newBlog).expect(200)
+})
+
+test("try to update a blog with invalid data", async () =>{
+    let blog = (await api.get("/api/blogs")).body[0]
+    let id = blog.id
+
+    let newBlog = {
+        title:"something",
+        url:"https://something.someone.com",
+        likes: "hi"
+    }
+    await api.put(`/api/blogs/${id}`).send(newBlog).expect(400)
+})
+
+test("try to update a blog with invalid id", async () =>{
+    let id = '15315321'
+
+    let newBlog = {
+        title:"something",
+        url:"https://something.someone.com",
+        likes: 30
+    }
+
+    await api.put(`/api/blogs/${id}`).send(newBlog).expect(400)
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
